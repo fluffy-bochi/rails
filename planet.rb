e60d3b3c5854f432764fb4cc34d8842a180d404f
fluffy-bochi <yunana1220@gmail.com>
2025-08-22 11:29:10 +0000

    chapter1 end

---
+++ planet.rb
@@ -0,0 +1,7 @@
+planets = {'mercury'=>'水星', 'venus'=>'金星', 'earth'=>'地球', 'mars'=>'火星', 'jupiter'=>'木星', 'saturn'=>'土星'}
+puts planets['mercury']
+puts planets['venus']
+puts planets['earth']
+puts planets['mars']
+puts planets['jupiter']
+puts planets['saturn']
